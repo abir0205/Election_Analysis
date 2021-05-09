bf1fbf5fceae0d1d5136c3183512ae852d1b8581
Abir Hossain <Ahossai7@binghamton.edu>
2021-05-09 19:14:15 -0400

    added more

--- PyPoll.py
+++ PyPoll.py
@@ -36,27 +36,33 @@ with open(file_to_load) as election_data:
     for row in file_reader:
         # 2. Add to the total vote count.
         total_votes += 1
-    # Print the candidate name from each row
+        # Print the candidate name from each row
         candidate_name = row[2]
         if candidate_name not in candidate_options:
             # Add the candidate name to the candidate list.
             candidate_options.append(candidate_name)
             # 2. Begin tracking that candidate's vote count.
             candidate_votes[candidate_name] = 0
-            # Add a vote to that candidate's count.
-            candidate_votes[candidate_name] += 1
-        # Add a vote to that candidate's count
+
+    # Add a vote to that candidate's count
         candidate_votes[candidate_name] += 1
 
-# 3. Print the total votes.
-# print(total_votes)
-# # Print the candidate list.
-# print(candidate_options)
-# # Print the candidate vote dictionary.
-# print(candidate_votes)
+# Save the results to our text file.
+with open(file_to_save, "w") as txt_file:
+    # Print the final vote count to the terminal.
+    election_results = (
+        f"\nElection Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"-------------------------\n")
+    print(election_results, end="")
+    # Save the final vote count to the text file.
+    txt_file.write(election_results)
 
-# Determine the percentage of votes for each candidate by looping through the counts.
-# 1. Iterate through the candidate list.
+
+
+    # Determine the percentage of votes for each candidate by looping through the counts.
+    # 1. Iterate through the candidate list.
     for candidate_name in candidate_votes:
         # 2. Retrieve vote count of a candidate.
         votes = candidate_votes[candidate_name]
@@ -74,11 +80,19 @@ with open(file_to_load) as election_data:
             winning_percentage = vote_percentage
             # And, set the winning_candidate equal to the candidate's name.
             winning_candidate = candidate_name
-    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
-    winning_candidate_summary = (
-    f"-------------------------\n"
-    f"Winner: {winning_candidate}\n"
-    f"Winning Vote Count: {winning_count:,}\n"
-    f"Winning Percentage: {winning_percentage:.1f}%\n"
-    f"-------------------------\n")
-print(winning_candidate_summary)
+        # print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+        # Winning Candidate Summary
+        winning_candidate_summary = (
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"-------------------------\n")
+
+        candidate_results = (f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        # Print each candidate, their voter count, and percentage to the terminal.
+        print(candidate_results)
+        #  Save the candidate results to our text file.
+        txt_file.write(candidate_results)
+        # print(winning_candidate_summary)
